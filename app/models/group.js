71f41c3f8a697487446ac19037660e6805c912c3
ChasevanHekken <cvh1717@gmail.com>
2015-02-24 11:37:34 -0700

    group model correct

--- app/models/group.js
+++ app/models/group.js
@@ -4,13 +4,13 @@ let Group = DS.Model.extend({
   name: DS.attr("string"),
   description: DS.attr("string"),
   admin_id: DS.attr("number"),
-  groups: DS.hasMany("group", {async: true})
+  users: DS.hasMany("user", {async: true})
 });
 
 Group.reopenClass({
   FIXTURES: [
     {
-      "id": 2,
+      "id": "2",
       "name": "this good name",
       "description": "this good description"
     }
