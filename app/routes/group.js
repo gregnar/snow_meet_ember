a205f4da1bc8eca7e9da7cba847c233e2f0614b5
worace <horace.d.williams@gmail.com>
2015-02-25 14:43:55 -0700

    remove some console debugging

--- app/routes/group.js
+++ app/routes/group.js
@@ -2,10 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    console.log("groups model hook with params", params);
     var group = this.store.find('group', params.group_id)
-    console.log("group..................");
-    window.group = group;
     return group;
   }
 });
--- app/routes/trip.js
+++ app/routes/trip.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    console.log("trips model hook with params", params.trip_id);
     var trip = this.store.find('trip', params.trip_id);
     return trip;
   }
