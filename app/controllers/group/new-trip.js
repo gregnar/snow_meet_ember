0a8a4f480fabb37e5f6ab31d698ae62c6401fa44
Timothy Mee <tjmee90@gmail.com>
2015-02-27 11:41:55 -0700

    trip to group relation isnt working right now for the saving of the trip

--- app/controllers/group/new-trip.js
+++ app/controllers/group/new-trip.js
@@ -4,16 +4,18 @@ export default Ember.Controller.extend({
   actions: {
 
     saveTrip: function () {
-      console.log('hit action')
       var name     = this.get('name');
       var id       = this.get('id');
       var location = this.get('location');
+      var group    = this.get('group');
       var return_time    = this.get('return_time');
       var departure_time = this.get('departure_time');
 
       this.store.createRecord('trip', {
         name: name,
         location: location,
+        id: id,
+        group: group,
         return_time: return_time ,
         departure_time: departure_time
       }).save().then(function (trip) {
