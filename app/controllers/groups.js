debf61064ffd281772f4cfaa11dbe1fc87001b02
Timothy Mee <tjmee90@gmail.com>
2015-02-27 09:52:59 -0700

    create group as a fixture works, does not persist over

--- app/controllers/groups.js
+++ app/controllers/groups.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+
+  // model: function () {
+  //   return this.store.find('group');
+  // }
+
+  // actions: {
+
+  //     numberOfGroups: function () {
+  //     return this.get('content').length;
+  //   }.property('content.[]')
+  // }
 });
--- app/controllers/groups/new.js
+++ app/controllers/groups/new.js
@@ -7,10 +7,12 @@ export default Ember.Controller.extend({
     saveGroup: function () {
       var name        = this.get('name');
       var description = this.get('description');
+      var id = this.get('id');
 
       this.store.createRecord('group', {
         name: name,
-        description: description
+        description: description,
+        id: id
       }).save().then(function (group) {
         this.transitionToRoute('group', group);
       }.bind(this));
