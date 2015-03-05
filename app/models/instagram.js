10e2ba943827f3ce22734cc68280951d86613e35
Timothy Mee <tjmee90@gmail.com>
2015-03-05 09:42:27 -0700

    instagram data from fixtures now shows on the trip show page

--- app/models/instagram.js
+++ app/models/instagram.js
@@ -1,24 +1,26 @@
 import DS from 'ember-data';
 
-let Insta =DS.Model.extend({
+let Instagram =DS.Model.extend({
   user_name:   DS.attr("string"),
   capition:    DS.attr("string"),
   profile_pic: DS.attr("string"),
   image_url:   DS.attr("string"),
   user: DS.belongsTo("user", {async: true}),
+  trip: DS.belongsTo("trip", {async: true}),
 });
 
-Insta.reopenClass({
+Instagram.reopenClass({
   FIXTURES: [
     {
-      "id": 1,
-      "user_name": "timtim",
-      "capition": "I am wicked cool",
-      "profile_pic": "http://news.bbcimg.co.uk/media/images/72015000/jpg/_72015753_72015747.jpg",
-      "image_url": "http://tong.visitkorea.or.kr/cms/resource/45/693245_image2_1.jpg",
-      "user": 1,
-    }
-  ]
-});
+        "id": 1,
+        "user_name": "timtim",
+        "capition": "I am wicked cool",
+        "profile_pic": "http://news.bbcimg.co.uk/media/images/72015000/jpg/_72015753_72015747.jpg",
+        "image_url": "http://tong.visitkorea.or.kr/cms/resource/45/693245_image2_1.jpg",
+        "user": 1,
+        "trip": 1,
+      }
+    ]
+  });
 
-export default Insta;
+  export default Instagram;
--- app/routes/trip.js
+++ app/routes/trip.js
@@ -4,4 +4,9 @@ export default Ember.Route.extend({
   model: function(params) {
     return this.store.find('trip', params.trip_id);
   }
+  // ,
+  // afterModel: function(trip) {
+  //   console.log("this is the trip" , trip)
+  //   this.transitionTo('trip.instagrams', trip.get('instagrams'));
+  // }
 });
