5c65fe7e890f2406ccbb56994f1c42476ef8e6cf
Timothy Mee <tjmee90@gmail.com>
2015-03-05 09:48:55 -0700

    insta data from fixtures loads

--- app/routes/trip.js
+++ app/routes/trip.js
@@ -4,9 +4,4 @@ export default Ember.Route.extend({
   model: function(params) {
     return this.store.find('trip', params.trip_id);
   }
-  // ,
-  // afterModel: function(trip) {
-  //   console.log("this is the trip" , trip)
-  //   this.transitionTo('trip.instagrams', trip.get('instagrams'));
-  // }
 });
