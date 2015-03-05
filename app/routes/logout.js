66942255a5afdbfc7b7429c37995b1f13ebbb954
worace <horace.d.williams@gmail.com>
2015-03-04 20:48:03 -0700

    working toward logout

--- app/routes/logout.js
+++ app/routes/logout.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model: function() {
+      console.log("Got Logout Model hook");
       $.removeCookie('authToken');
       this.transitionTo("splash");
     }
--- app/routes/splash.js
+++ app/routes/splash.js
@@ -3,11 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
     if(this.token()) {
+      var controller = this;
       return $.ajax({url: "http://lvh.me:3000/api/v1/sessions.json",
                       type: "GET",
                       data: {token: this.token()}}
               ).done(function(data) {
                 console.log("logged in user using token, got data ", data);
+                controller.transitionTo("user", data);
                 return data;
               }).fail(function(data) {
                 console.log("failed to log in, got data", data);
@@ -16,14 +18,6 @@ export default Ember.Route.extend({
     } else {
       return null;
     }
-  },
-  afterModel: function(user) {
-    console.log("after model got user ", user);
-    if(user) {
-      this.transitionTo("user", user);
-    }
-  },
-  goToUserShow: function() {
   },
   token: function() {
     return $.cookie("authToken");
