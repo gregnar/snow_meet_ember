85c9d9e33a63bf7bf55584358e0b3046e0e22914
Timothy Mee <tjmee90@gmail.com>
2015-03-05 19:09:08 -0700

    removed the fixtures from user.js

--- app/models/user.js
+++ app/models/user.js
@@ -11,39 +11,4 @@ let User = DS.Model.extend({
   rsvps: DS.hasMany("rsvp", {async: true})
 });
 
-User.reopenClass({
-  FIXTURES: [
-    {
-      "id": 1,
-      "twitter_name": "@superCool",
-      "insta_name": "superCool",
-      "email": "superCool@example.com",
-      "first_name": "Pat",
-      "last_name": "Mee",
-      "password_digest": "password",
-      "groups": [2]
-    },
-    {
-      "id": 2,
-      "twitter_name": "@McDonalds",
-      "insta_name": "Yamhumgry",
-      "email": "burger@example.com",
-      "first_name": "Bandof",
-      "last_name": "Brothers",
-      "password_digest": "password",
-      "groups": [2,1]
-    },
-    {
-      "id": 3,
-      "twitter_name": "@inotalk",
-      "insta_name": "noseeeither",
-      "email": "internet@example.com",
-      "first_name": "EMANTSRIF",
-      "last_name": "SWIMS",
-      "password_digest": "password",
-      "groups": [1,2]
-    }
-  ]
-});
-
 export default User;
