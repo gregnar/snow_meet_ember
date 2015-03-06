64a522a380195a4491bb6fa15e942a8527f3c65d
ChasevanHekken <cvh1717@gmail.com>
2015-03-05 22:03:58 -0700

    still not working but saving changes

--- app/controllers/group/new-trip.js
+++ app/controllers/group/new-trip.js
@@ -8,11 +8,22 @@ export default Ember.Controller.extend({
       var id       = this.get('id');
       var city     = this.get('city');
       var state    = this.get('state');
-      var group    = this.get('group_id');
+      var group    = this.store.find('group');
       var return_time    = this.get('return_time');
       var departure_time = this.get('departure_time');
 
-      this.store.createRecord('trip', {
+      // var post = this.store.createRecord('post', {
+      //   title: 'Rails is Omakase',
+      //   body: 'Lorem ipsum'
+      // });
+
+      // this.store.find('user', 1).then(function(user) {
+      //   post.set('author', user);
+      // });
+
+      // debugger;
+
+      var new_trip = this.store.createRecord('trip', {
         name: name,
         city: city,
         state: state,
@@ -20,7 +31,16 @@ export default Ember.Controller.extend({
         group: group,
         return_time: return_time ,
         departure_time: departure_time
-      }).save().then(function (trip) {
+      });
+
+      // debugger;
+
+      // new_trip.set('group', group.id);
+      new_trip.set('group.id', null);
+
+
+
+      new_trip.save().then(function (trip) {
         this.transitionToRoute('group.trip', trip);
       }.bind(this));
     }
