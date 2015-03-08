8cedc1a7e7a2616f59b6a99c70fa07fd8be6a538
Timothy Mee <tjmee90@gmail.com>
2015-03-07 18:03:09 -0700

    fixed the group controller to save group from user show apge and transition to group show page

--- app/controllers/user/new-group.js
+++ app/controllers/user/new-group.js
@@ -12,16 +12,8 @@ export default Ember.Controller.extend({
         description: description,
        });
 
-      var user = this.get('model');
-      new_group.set('user', user);
-
-      var groupUser= this.get('groupUser').createRecord({
-        group_id: user_id
-      });
-      this.get('groupUser').pushObject(groupUser);
-
       new_group.save().then(function (group) {
-        this.transitionToRoute('user');
+        this.transitionToRoute('group', group);
       }.bind(this));
     }
   }
