7c4f9dcb5cf5ad198b2ba49f101f2491bec32224
Greg Narajka <gregnar@gmail.com>
2015-03-08 19:09:48 -0600

    fix index for simple-auth redirects

--- app/routes/group.js
+++ app/routes/group.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model: function(params) {
-    console.log("HELLOOOOOO", this.get('currentUserGetter').currentUser());
     return this.store.find('group', params.group_id);
   },
   afterModel: function(group) {
